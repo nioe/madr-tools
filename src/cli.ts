0cb03264e7deb28550fffdb5af718d128ca0bfe2
Nicolas Oeschger <nicolas.oeschger@niver.ch>
2024-07-08 11:56:10 +0200

    feat: Bundle dependencies to make CLI work without installing additional packages

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import yargs from 'yargs';
+import yargs from 'yargs/yargs';
 import {hideBin} from 'yargs/helpers';
 
 yargs(hideBin(process.argv)).scriptName('madr').commandDir('commands').demandCommand().strict().alias({h: 'help'}).argv;
