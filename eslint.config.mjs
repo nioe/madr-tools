731aeabb500ce549d8299a020332e7e9db7b8e32
Nicolas Oeschger <nicolas.oeschger@niver.ch>
2024-09-16 10:45:49 +0200

    feat: Change built package from CJS to ESM

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+  files: ['src/**/*.ts'],
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  rules: {},
+});
--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 import yargs from 'yargs/yargs';
 import {hideBin} from 'yargs/helpers';
+import * as indexCommand from './commands/index';
+import * as initCommand from './commands/init';
+import * as newCommand from './commands/new';
+import {CommandModule} from 'yargs';
 
-yargs(hideBin(process.argv)).scriptName('madr').commandDir('commands').demandCommand().strict().alias({h: 'help'}).argv;
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+yargs(hideBin(process.argv))
+  .scriptName('madr')
+  .command([indexCommand, initCommand, newCommand] as Array<CommandModule>)
+  .demandCommand()
+  .strict()
+  .version('1.1.3') // x-release-please-version
+  .alias({h: 'help', v: 'version'}).argv;
