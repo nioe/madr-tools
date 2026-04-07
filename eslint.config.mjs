aad6cb80ed2e815bc5abe2faa1daf5f5bb69227e
Nicolas Oeschger <nicolas@lupiq.com>
2026-04-07 12:12:36 +0200

    build: Update all dependencies and refactor code for major changes (#250)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,5 @@
 import eslint from '@eslint/js';
+import {defineConfig} from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  files: ['src/**/*.ts'],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-  rules: {},
-});
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended);
